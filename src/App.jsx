1012fd072a9713b0b9a9e4c56c1cbae7c4777515
Alejandro-Izcalbaceta <alejandro.izcalbaceta@ipiam.com.mx>
2025-03-25 23:07:01 -0600

    [ADD]Register user

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import AddClientSurvey from './pages/Add Client Survey/AddClientSurvey'
 import PresalesInformation from './pages/Presales Information/PresalesInformation'
 import AgentsInformation from './pages/Agents Information/AgentsInformation'
 import PotentialAgents from './pages/Potential Agents/PotentialAgents'
+import RegisterUser from './pages/RegisterUser/RegisterUser'
 import './App.css'
 
 function App() {
@@ -42,6 +43,7 @@ function App() {
           <Route path="/presales-information" element={<PresalesInformation/>} />
           <Route path="/agents-information" element={<AgentsInformation/>} />
           <Route path="/potential-agents" element={<PotentialAgents/>} />
+          <Route path="/register-user" element={<RegisterUser/>} />
         </Routes>
       </main>
     </div>
