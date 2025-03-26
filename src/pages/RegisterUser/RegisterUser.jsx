5f1c7460bda0a1fe3da507fb914e0bb96143732d
Alejandro-Izcalbaceta <alejandro.izcalbaceta@ipiam.com.mx>
2025-03-26 16:39:13 -0600

    [UPDATE]Register user updated

--- src/pages/RegisterUser/RegisterUser.jsx
+++ src/pages/RegisterUser/RegisterUser.jsx
@@ -1,17 +1,84 @@
 "use client"
 
+import Swal from "sweetalert2"
 import React, { useState } from "react"
-import {Box, Container, Breadcrumbs, Typography, Link, Paper, TextField, Select, MenuItem, Button, InputLabel, FormControl} from "@mui/material"
+import {
+  Box,
+  Container,
+  Breadcrumbs,
+  Typography,
+  Link,
+  Paper,
+  Button,
+} from "@mui/material"
 import {
   Home as HomeIcon,
-  AccountCircle as PersonIcon} from "@mui/icons-material"
+  AccountCircle as UserIcon,
+  Mail as MailIcon,
+  Lock as LockIcon,
+  Verified as VerifiedIcon,
+} from "@mui/icons-material"
 import styles from "./registerUser.module.css"
 
+
 const RegisterUser = () => {
-  
+
   const permission = ["Administrador", "General"]
 
-    const initialFormData = {
+  const [formData, setFormData] = useState({
+    userName: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    permissions: "",
+    password: "",
+    confirmPassword: "",
+  })
+
+  const validatePassword = () => {
+    return formData.password === formData.confirmPassword
+  }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }))
+  }
+
+  const handleRegister = () => {
+
+    if (!isFormValid()) {
+      Swal.fire({
+          title: "Error",
+          text: "Por favor, complete todos los campos requeridos.",
+          icon: "error",
+          confirmButtonText: "Ok",
+      });
+      return;
+  }
+
+    if (!validatePassword()) {
+      Swal.fire({
+        title: "Error",
+        text: "Las contraseñas no coinciden.",
+        icon: "error",
+        confirmButtonText: "Ok",
+      })
+      return
+    }
+    Swal.fire({
+      title: "¡Éxito!",
+      text: "El usuario ha sido registrado correctamente.",
+      icon: "success",
+      confirmButtonText: "Ok",
+    })
+    resetForm()
+  }
+
+  const resetForm = () => {
+    setFormData({
       userName: "",
       firstName: "",
       lastName: "",
@@ -19,26 +86,22 @@ const RegisterUser = () => {
       permissions: "",
       password: "",
       confirmPassword: "",
-    }
+    })
+  }
 
-    const [formData, setFormData] = useState(initialFormData)
-  
-    const handleChange = (e) => {
-      setFormData({ ...formData, [e.target.name]: e.target.value })
-    }
-  
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      if (formData.password !== formData.confirmPassword) {
-        alert("Passwords do not match")
-        return
-      }
-      alert("User created successfully!")
-      setFormData(initialFormData)
-      console.log(formData)
-    }
+  const isFormValid = () => {
+    return (
+      formData.userName &&
+      formData.firstName &&
+      formData.lastName &&
+      formData.email &&
+      formData.permissions &&
+      formData.password &&
+      formData.confirmPassword
+    )
+  }
 
-  return(
+  return (
     <Container maxWidth="xl" className={styles.container}>
       <Box className={styles.header}>
         <div className={styles.headerLeft}>
@@ -50,107 +113,122 @@ const RegisterUser = () => {
               <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
               Home
             </Link>
-            <Typography color="text.primary">Register user</Typography>
+            <Typography color="text.primary">Register User</Typography>
           </Breadcrumbs>
         </div>
       </Box>
 
-      <div className={styles.sectionCardContainer}>
-      <Paper elevation={0} className={styles.sectionCard}>
-      <PersonIcon sx={{ mr: 0.5 }}/>
-        <Typography variant="h6" className={styles.sectionTitle}>
-          Register user
-        </Typography>
-        <Typography variant="body2" color="text.secondary" sx={{ mb: 3 }}>
-            Create a new user 
-        </Typography>
+      <Box display="flex" justifyContent="center" alignItems="center" minHeight="10vh">
+        <Paper elevation={3} style={{ padding: "32px", width: "800px", margin: "0 auto" }}>
+          <Typography variant="h5" align="center" style={{ marginBottom: "24px" }}>
+            New User Information
+          </Typography>
 
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <TextField
-            label="User Name"
-            name="userName"
-            value={formData.userName}
-            onChange={handleChange}
-            fullWidth
-            margin="normal"
-            required
-          />
-          <TextField
-            label="First Name"
-            name="firstName"
-            value={formData.firstName}
-            onChange={handleChange}
-            fullWidth
-            margin="normal"
-            required
-          />
-          <TextField
-            label="Last Name"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleChange}
-            fullWidth
-            margin="normal"
-            required
-          />
-          <TextField
-            label="Email"
-            name="email"
-            type="email"
-            value={formData.email}
-            onChange={handleChange}
-            fullWidth
-            margin="normal"
-            required
-          />
-          <FormControl fullWidth margin="normal" required>
-              <InputLabel id="permissions-label">Permissions</InputLabel>
-              <Select
-                labelId="permissions-label"
-                id="permissions" 
-                name="permissions"
-                value={formData.permissions}
-                onChange={handleChange}
-                label="Permissions" 
-              >
-                <MenuItem value="">Select Permissions</MenuItem>
-                {permission.map((perm) => (
-                  <MenuItem key={perm} value={perm}>
-                    {perm}
-                  </MenuItem>
+          <div className={styles.formRow}>
+            <div className={styles.formCol}>
+              <UserIcon className={styles.iconTitle}/>
+              <label className={`${styles.formLabel} ${styles.requiredField}`}>User Name</label>
+              <input 
+              type="text" 
+              name="userName" 
+              value={formData.userName} 
+              onChange={handleInputChange} 
+              placeholder="User name" 
+              className={styles.formInput}
+              required />
+            </div>
+            <div className={styles.formCol}>
+            <UserIcon className={styles.iconTitle}/>
+            <label className={`${styles.formLabel} ${styles.requiredField}`}>First Name</label>
+              <input 
+              type="text" 
+              name="firstName" 
+              value={formData.firstName} 
+              onChange={handleInputChange} 
+              placeholder="First name" 
+              className={styles.formInput} 
+              required/>
+            </div>
+          </div>
+
+          <div className={styles.formRow}>
+            <div className={styles.formCol}>
+            <UserIcon className={styles.iconTitle}/>
+            <label className={`${styles.formLabel} ${styles.requiredField}`}>Last Name</label> 
+              <input 
+              type="text" 
+              name="lastName" 
+              value={formData.lastName} 
+              onChange={handleInputChange} 
+              placeholder="Last name" 
+              className={styles.formInput} 
+              required/>
+            </div>
+            <div className={styles.formCol}>
+            <MailIcon className={styles.iconTitle}/>
+            <label className={`${styles.formLabel} ${styles.requiredField}`}>Email</label>
+              <input 
+              type="text" 
+              name="email" 
+              value={formData.email} 
+              onChange={handleInputChange} 
+              placeholder="Email" 
+              className={styles.formInput} 
+              required/>
+            </div>
+          </div>
+
+          <VerifiedIcon className={styles.iconTitle}/>
+          <label className={`${styles.formLabel} ${styles.requiredField}`}>Select permission</label>
+              <select 
+              name="permissions" 
+              value={formData.permissions} 
+              onChange={handleInputChange} 
+              className={styles.formSelect}
+              required>
+                <option value="">Select a permission</option>
+                {permission.map((item) => (
+                  <option key={item} value={item}>
+                    {item}
+                  </option>
                 ))}
-              </Select>
-            </FormControl>
-          <TextField
-            label="Password"
-            name="password"
-            type="password"
-            value={formData.password}
-            onChange={handleChange}
-            fullWidth
-            margin="normal"
-            required
-          />
-          <TextField
-            label="Confirm Password"
-            name="confirmPassword"
-            type="password"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-            fullWidth
-            margin="normal"
-            required
-          />
-          <Button type="submit" variant="contained" color="primary">
-            Create
+              </select>
+            
+
+          <div className={styles.formRow}>
+            <div className={styles.formCol}>
+            <LockIcon className={styles.iconTitle}/>
+            <label className={`${styles.formLabel} ${styles.requiredField}`}>Password</label>
+              <input 
+              type="password" 
+              name="password" 
+              value={formData.password} 
+              onChange={handleInputChange} 
+              placeholder="Password" 
+              className={styles.formInput}
+              required />
+            </div>
+            <div className={styles.formCol}>
+            <LockIcon className={styles.iconTitle}/>
+            <label className={`${styles.formLabel} ${styles.requiredField}`}>Confirm password</label>
+              <input 
+              type="password" 
+              name="confirmPassword" 
+              value={formData.confirmPassword} 
+              onChange={handleInputChange} 
+              placeholder="Confirm password" 
+              className={styles.formInput} 
+              required/>
+            </div>
+          </div>
+
+          <Button variant="contained" color="primary" onClick={handleRegister}  fullWidth style={{ marginTop: "24px", borderRadius: "20px",backgroundColor: "#6362e7" }}>
+            Submit
           </Button>
-        </form>
-
-
-      </Paper>
-    </div>
+        </Paper>
+      </Box>
     </Container>
   )
-
 }
+
 export default RegisterUser
